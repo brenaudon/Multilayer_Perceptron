1ca74372ccc5208db97cf7b553ccaa1e95014a3a
brenaudon <brenaudon9@gmail.com>
2025-02-28 15:57:52 +0100

    fix: comparison script adapted to new folders organization

--- src/models_comparison.py
+++ src/models_comparison.py
@@ -1,26 +1,47 @@
 import numpy as np
 import glob
 import sys
+import os
+import argparse
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
+    # Create an argument parser with a proper description
+    parser = argparse.ArgumentParser(description='Plot training history of models')
+
+    # Add optional argument for specifying model names
+    parser.add_argument('model_names', nargs='*', help='Names of the models to plot (leave empty to plot all)')
+
+    # Parse arguments
+    args = parser.parse_args()
+
+    # Get the absolute path of the current script
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    models_path = os.path.join(script_dir, '../models')
+
     # Get filenames
     # All model if no argument is passed
     if len(sys.argv) == 1:
-        filenames_training_history = glob.glob('*_training_history.npy')
-        filenames_validate_history = glob.glob('*_validate_history.npy')
+        filenames_training_history_array = glob.glob(f'{models_path}/*/*_training_history.npy')
+        filenames_validate_history_array = glob.glob(f'{models_path}/*/*_validate_history.npy')
+        # Create dictionaries with model name as key (filename.split('/')[-2] is the model name by folder normalization)
+        filenames_training_history = {filename.split('/')[-2]: filename for filename in filenames_training_history_array}
+        filenames_validate_history = {filename.split('/')[-2]: filename for filename in filenames_validate_history_array}
     else:
-        filenames_training_history = []
-        filenames_validate_history = []
+        filenames_training_history = {}
+        filenames_validate_history = {}
         for model_name in sys.argv[1:]:
-            filenames_training_history.append(model_name + '_training_history.npy')
-            filenames_validate_history.append(model_name + '_validate_history.npy')
+            if not os.path.exists(f'{script_dir}/../models/{model_name}'):
+                print(f"Warning: Model not found: {model_name}")
+                continue
+            filenames_training_history[model_name] = f'{models_path}/{model_name}/{model_name}_training_history.npy'
+            filenames_validate_history[model_name] = f'{models_path}/{model_name}/{model_name}_validate_history.npy'
 
     num_metrics = 0
-    sample_history = np.load(filenames_training_history[0], allow_pickle=True).item()
+    sample_history = None
     # find the history with the most metrics
-    for filename in filenames_training_history:
-        sample_history = np.load(filename, allow_pickle=True).item()
+    for model_name in filenames_training_history.keys():
+        sample_history = np.load(filenames_training_history[model_name], allow_pickle=True).item()
         if len(sample_history.keys()) > num_metrics:
             num_metrics = len(sample_history.keys())
 
@@ -32,16 +53,17 @@ if __name__ == "__main__":
     axes = axes.flatten()  # Flatten to index easily
 
     # Loop through models
-    for filename in filenames_training_history:
-        model_name = filename.split('_')[0]
+    for model_name in filenames_training_history.keys():
         try:
-            training_history = np.load(filename, allow_pickle=True).item()
+            training_history = np.load(filenames_training_history[model_name], allow_pickle=True).item()
         except FileNotFoundError:
             print(f"Unknown model name: {model_name}")
             continue
 
+        validate_history = np.load(filenames_validate_history[model_name], allow_pickle=True).item()
 
-        validate_history = np.load(model_name + '_validate_history.npy', allow_pickle=True).item()
+        if sample_history is None:
+            sample_history = training_history
 
         # Loss subplot (first one)
         line_train, = axes[0].plot(training_history['loss'][:], label=model_name + ' training')  # Store line object
